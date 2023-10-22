8e251965d0a48aaf867deb43581da34431c17954
conghung43 <conghung43@gmail.com>
2023-10-22 14:47:17 +0800

    them axis cho bieu do

--- WifiConnectServerTurnOnLed.ino
+++ WifiConnectServerTurnOnLed.ino
@@ -9,9 +9,11 @@ ESP8266WebServer server(80);
 const int ledPin = 2;
 int ledState = LOW;
 int data[] = {3, 7, 2, 5, 8, 4};
+int secondaryData[] = {3, 10, 12, 17, 25, 29};
 
 void handleRoot() {
   String html = "<html><head><meta charset='UTF-8'></head><body>";
+  html += "<style>h1 {text-align: center;}</style>";
   html += "<h1> BẢNG THỐNG KÊ CÔNG SUẤT SỬ DỤNG ĐIỆN </h1>";
   html += "<p> TEST trạng thái BUILDIN LED : " + String(ledState == HIGH ? "On" : "Off") + "</p>";
   html += "<p><a href='/toggle'>Bật/Tắt LED</a></p>";
@@ -21,13 +23,23 @@ void handleRoot() {
   html += "var ctx = document.getElementById('histogramChart').getContext('2d');";
   html += "var data = {";
   html += "  labels: ['Ngày 1', 'Ngày 2', 'Ngày 3', 'Ngày 4', 'Ngày 5', 'Ngày 6'],";
-  html += "  datasets: [{";
-  html += "    label: 'TEST Lưu lượng sử dụng điện',";
-  html += "    data: " + String("[") + data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5] + "],";
-  html += "    backgroundColor: 'rgba(75, 192, 192, 0.2)',";
-  html += "    borderColor: 'rgba(75, 192, 192, 1)',";
-  html += "    borderWidth: 1";
-  html += "  }]";
+  html += "  datasets: [";
+  html += "    {";
+  html += "      type: 'bar',";
+  html += "      label: 'Công suất hằng ngày(kWh)',";
+  html += "      data: " + String("[") + data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5] + "],";
+  html += "      backgroundColor: 'rgba(75, 192, 192, 0.2)',";
+  html += "      borderColor: 'rgba(75, 192, 192, 1)',";
+  html += "      borderWidth: 1";
+  html += "    },";
+  html += "    {";
+  html += "      type: 'line',";
+  html += "      label: 'Tổng lượng dùng từ đầu tháng(kWh)',";
+  html += "      data: " + String("[") + secondaryData[0] + ", " + secondaryData[1] + ", " + secondaryData[2] + ", " + secondaryData[3] + ", " + secondaryData[4] + ", " + secondaryData[5] + "],";
+  html += "      borderColor: 'rgba(255, 99, 132, 1)',";
+  html += "      yAxisID: 'secondaryYAxis'";
+  html += "    }";
+  html += "  ]";
   html += "};";
   html += "var myChart = new Chart(ctx, {";
   html += "  type: 'bar',";
@@ -35,7 +47,19 @@ void handleRoot() {
   html += "  options: {";
   html += "    scales: {";
   html += "      y: {";
-  html += "        beginAtZero: true";
+  html += "        beginAtZero: true,";
+  html += "        title: {";
+  html += "          display: true,";
+  html += "          text: 'Công suất hằng ngày(kWh)'";
+  html += "        }";
+  html += "      },";
+  html += "      secondaryYAxis: {";
+  html += "        position: 'right',";
+  html += "        beginAtZero: true,";
+  html += "        title: {";
+  html += "          display: true,";
+  html += "          text: 'Tổng lượng dùng từ đầu tháng(kWh)'";
+  html += "        }";
   html += "      }";
   html += "    }";
   html += "  }";
@@ -44,6 +68,7 @@ void handleRoot() {
   html += "</body></html>";
   server.send(200, "text/html", html);
 }
+
 
 
 void handleToggle() {
