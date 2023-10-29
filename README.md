d808b5f0196a382deefae2a7ee7a289cfa36abe7
Kai Nguyen <conghung43@gmail.com>
2023-10-29 19:10:01 +0800

    add onfocus

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ESP8266_Server_CongToDien
 
+# Save/read json
+
+Select Sketch > Include Library > Manage Libraries... from the Arduino IDE menus.
+Wait for the download to finish.
+In the "Filter your search..." box, type "arduinojson".
--- WifiConnectServerTurnOnLed/WifiConnectServerTurnOnLed.ino
+++ WifiConnectServerTurnOnLed/WifiConnectServerTurnOnLed.ino
@@ -1,8 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h> 
+#include <FS.h> // Include the SPIFFS library to save data to flash
+#include <ArduinoJson.h> // Include the ArduinoJSON library
+
+
 const char* ssid = "Steve'sWIFI";
 const char* password = "ji7ewg1k";
+IPAddress subnet(255, 255, 0, 0);			            // Subnet Mask
+IPAddress gateway(192, 168, 1, 1);			            // Default Gateway
+IPAddress local_IP(192, 168, 1, 184);			        // Static IP Address for ESP8266
 
 ESP8266WebServer server(80);
 
@@ -40,6 +47,95 @@ String GetDataString(char* inputData[], String key){
   return dataString;
 }
 
+// Save Json
+void saveJsonData() {
+  if (SPIFFS.begin()) {
+    // Create the JSON object with the specified structure
+    StaticJsonDocument<2048> jsonData;
+    JsonObject dataObj = jsonData.createNestedObject("data");
+    
+    JsonObject day1 = dataObj.createNestedObject("day1");
+    day1["key1"] = "value1";
+    day1["key2"] = "value2";
+    day1["key3"] = "value3";
+
+    JsonObject day2 = dataObj.createNestedObject("day2");
+    day2["key1"] = "value1";
+    day2["key2"] = "value2";
+    day2["key3"] = "value3";
+
+    // Open the file for writing
+    File file = SPIFFS.open("/data.json", "w");
+    if (!file) {
+      Serial.println("Failed to open file for writing");
+      return;
+    }
+
+    // Serialize the JSON data and write it to the file
+    if (serializeJson(jsonData, file) == 0) {
+      Serial.println("Failed to write to file");
+    }
+
+    // Close the file
+    file.close();
+    Serial.println("Data saved to data.json");
+  } else {
+    Serial.println("Failed to mount SPIFFS filesystem");
+  }
+}
+
+void readJsonData() {
+  if (SPIFFS.begin()) {
+    // Open the file for reading
+    File file = SPIFFS.open("/data.json", "r");
+    if (!file) {
+      Serial.println("Failed to open file for reading");
+      return;
+    }
+
+    // Get the file size
+    size_t fileSize = file.size();
+
+    // Allocate a buffer to hold the JSON data
+    std::unique_ptr<char[]> buffer(new char[fileSize]);
+
+    // Read the file into the buffer
+    file.readBytes(buffer.get(), fileSize);
+
+    // Parse the JSON data
+    DynamicJsonDocument jsonData(2048); // Adjust the size as needed
+    DeserializationError error = deserializeJson(jsonData, buffer.get());
+
+    if (error) {
+      Serial.print("JSON parsing error: ");
+      Serial.println(error.c_str());
+    } else {
+      // Successfully parsed JSON
+      Serial.println("Data from data.json:");
+
+      // Access JSON data, for example:
+      const char* key1 = jsonData["data"]["day1"]["key1"];
+      const char* key2 = jsonData["data"]["day1"]["key2"];
+      const char* key3 = jsonData["data"]["day1"]["key3"];
+
+      Serial.print("key1: ");
+      Serial.println(key1);
+      Serial.print("key2: ");
+      Serial.println(key2);
+      Serial.print("key3: ");
+      Serial.println(key3);
+    }
+
+    // Close the file
+    file.close();
+  } else {
+    Serial.println("Failed to mount SPIFFS filesystem");
+  }
+}
+
+
+
+// Web design
 void handleRoot() {
 
   String html = "<html><head><meta charset='UTF-8'></head><body>";
@@ -54,6 +150,35 @@ void handleRoot() {
   html += "<input type='text' id='dataInput' placeholder='Nhập số liệu'>";
   html += "<button onclick='addData()'>Xác nhận</button>";
   html += "</div>";
+
+  // Thêm trường nhập và xử lý sự kiện vào form
+  html += "<form>";
+  html += "<label for='level1'>Nhập thông tin:</label>";
+  html += "<input type='text' id='level1' onfocus='textFieldFocus()' onblur='textFieldBlur(1)' >";
+  html += "<p id='status'>Status: Not Focused</p>";
+  html += "</form>";
+
+  // OnFocus text field
+  html += "<script>";
+  html += "function textFieldFocus() {";
+  html += "  document.getElementById('status').innerText = 'Status: Focused';";
+  html += "}";
+  html += "function textFieldBlur(id) {";
+  //html += "  document.getElementById('status').innerText = 'Status: Not Focused' + id;";
+
+  html += "  var textField = document.getElementById('level'+id);";
+  html += "  var inputValue = textField.value;";
+  html += "  document.getElementById('result').innerText = 'Result: ' + inputValue;";
+
+  html += "}";
+  html += "</script>";
+
+  //Remove Non digit
+  html += "<script>";
+  html += "function allowDigits(inputField) {";
+  html += "  inputField.value = inputField.value.replace(/\\D/g, '');";
+  html += "}";
+  html += "</script>";
 
   html += "<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js'></script>";
   html += "<script>";
@@ -126,8 +251,13 @@ void handleToggle() {
 }
 
 void setup() {
+  // if (WiFi.config(local_IP, gateway, subnet)) {
+  //   Serial.println("Static IP Configured");
+  // }
+  // else {
+  //   Serial.println("Static IP Configuration Failed");
+  // }
   Serial.begin(115200);
-  //WiFi.persistent( false );
   WiFi.persistent(false);
   WiFi.setSleepMode(WIFI_NONE_SLEEP);
   WiFi.begin(ssid, password);
@@ -151,6 +281,16 @@ void setup() {
   }
   Serial.println("mDNS responder started");
   MDNS.addService("http", "tcp", 80);
+
+  // Initialize SPIFFS
+  if (!SPIFFS.begin()) {
+    Serial.println("Failed to mount SPIFFS filesystem");
+    return;
+  }
+
+  // Save JSON data to a file
+  //saveJsonData();
+  //readJsonData();
 }
 
 void loop() {
